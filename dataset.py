07f5dde0f0c7d01c26737361a3f28e089d9a0e25
NiccoloCavagnero <48228170+NiccoloCavagnero@users.noreply.github.com>
2020-05-17 13:40:27 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,30 +1,25 @@
 from torchvision.datasets import VisionDataset, CIFAR100
 
 class Cifar100(VisionDataset):
-     def __init__(self, root, train, transform=None, target_transform=None):
-         super(Cifar100, self).__init__(root, transform=transform, target_transform=target_transform)
-         self.dataset = CIFAR100(root=root, train=train, download=True, transform=transform)
-         self.transform = transform
+    def __init__(self, root, train, transform=None, target_transform=None):
+        super(Cifar100, self).__init__(root, transform=transform, target_transform=target_transform)
+        self.dataset = CIFAR100(root=root, train=train, download=True, transform=transform)
 
-     def __getitem__(self, index):
-         image, label = self.dataset[index]
+    def __getitem__(self, index):
+        image, label = self.dataset[index]
+        return image, label
 
-         if self.transform is not None:
-             image = self.transform(image)
+    def __len__(self):
+        return len(self.dataset)
 
-         return image, label
-
-     def __len__(self):
-         return len(self.dataset)
-
-     def __getClassBatch__(self,classes):
-         '''
-         Returns indexes of images whose label is
-         in classes (-> Subset method)
-         '''
-         classes = set(classes)
-         batch = []
-         for idx,item in enumerate(self.dataset):
-             if item[1] in classes:
-                 batch.append(idx)
-         return batch
+    def __getClassBatch__(self,classes):
+        '''
+        Returns indexes of images whose label is
+        in classes (-> Subset method)
+        '''
+        classes = set(classes)
+        batch = []
+        for idx,item in enumerate(self.dataset):
+            if item[1] in classes:
+                batch.append(idx)
+        return batch
