438ec28dbf18349bf88d5fe915f2d31d828cb1fd
NiccoloCavagnero <48228170+NiccoloCavagnero@users.noreply.github.com>
2020-05-17 13:18:43 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,26 +1,30 @@
 from torchvision.datasets import VisionDataset, CIFAR100
 
-class Cifar100(VisionDataset):
-    def _init_(self, root, train, transform=None, target_transform=None):
-        super(Cifar100, self)._init_(root, train=train, transform=transform, target_transform=target_transform)
-        self.dataset = CIFAR100(root=root, train=train, download=True, transform=transform)
-        self.transform = transform
+ class Cifar100(VisionDataset):
+     def __init__(self, root, train, transform=None, target_transform=None):
+         super(Cifar100, self).__init__(root, transform=transform, target_transform=target_transform)
+         self.dataset = CIFAR100(root=root, train=train, download=True, transform=transform)
+         self.transform = transform
 
-    def _getitem_(self, index):
-        image, label = self.dataset[index]
-        if self.transform is not None:
-            image = self.transform(image)
-        return image, label
+     def __getitem__(self, index):
+         image, label = self.dataset[index]
 
-    def _len_(self):
-        return len(self.dataset)
+         if self.transform is not None:
+             image = self.transform(image)
 
-    # Returns indexes of images whose label is
-    # in classes (-> Subset method)
-    def _getClassBatch_(self,classes):
-        classes = set(classes)
-        batch = []
-        for idx,item in enumerate(self.dataset):
-            if item[1] in classes:
-                batch.append(idx)
-        return batch
+         return image, label
+
+     def __len__(self):
+         return len(self.dataset)
+
+     def __getClassBatch__(self,classes):
+         '''
+         Returns indexes of images whose label is
+         in classes (-> Subset method)
+         '''
+         classes = set(classes)
+         batch = []
+         for idx,item in enumerate(self.dataset):
+             if item[1] in classes:
+                 batch.append(idx)
+         return batch
