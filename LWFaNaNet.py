08276fac681aa99517c4629f6846922e4eeab86d
Alessio Serra <48280936+alessioserra@users.noreply.github.com>
2020-05-19 16:05:57 +0200

    Update and rename LWFaNaNet().py to LWFaNaNet.py
    improvements

---
+++ LWFaNaNet.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+from copy import deepcopy
+
+class aNaNet(nn.Module):
+
+    def __init__(self, num_classes=100):
+        super(aNaNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(128 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )    
+        self.nets = []
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+def _updateNet_(self,net,n_classes):
+        in_features = net.classifier[7].in_features
+        out_features = net.classifier[7].out_features
+        weight = net.classifier[7].weight.data
+        bias = net.classifier[7].bias.data
+
+        net.classifier[7] = nn.Linear(in_features, n_classes)
+        net.classifier[7].weight.data[:out_features] = weight
+        net.classifier[7].bias.data[:out_features] = bias
+
+        return net
+
+
+def ananet(progress=True, **kwargs):
+    model = aNaNet(**kwargs)
+    return model
+
+net = ananet()
