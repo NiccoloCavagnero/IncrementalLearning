e660592a5fb875bd4f256d514077961f80eebf38
NiccoloCavagnero <48228170+NiccoloCavagnero@users.noreply.github.com>
2020-06-12 01:51:59 +0200

    Update iCaRL.py

--- iCaRL.py
+++ iCaRL.py
@@ -370,7 +370,7 @@ class iCaRL():
       return new_exemplars
     
     def __confusionMatrix__(self,labels,predictions):
-      sns.heatmap(confusion_matrix(labels, predictions), annot=True, cmap='coolwarm')
+      sns.heatmap(confusion_matrix(labels, predictions), cmap='Spectral')
       plt.show()
       
     def __printTime__(self,t0):
