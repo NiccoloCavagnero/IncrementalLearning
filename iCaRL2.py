e17795d69b7274cd2779ced1168440c80e34a0ad
NiccoloCavagnero <48228170+NiccoloCavagnero@users.noreply.github.com>
2020-08-10 15:10:09 +0200

    Update iCaRL2.py

--- iCaRL2.py
+++ iCaRL2.py
@@ -20,88 +20,6 @@ class iCaRL2():
         self.params = params
         self.plot = plot
         self.teachers = []
-
-    def __NMEClassifier__(self,data,batch,exemplars,net,n_classes):
-      print(f'\n ### NME ###')
-      means = dict.fromkeys(np.arange(n_classes))
-      net.eval()
-
-      batch_map = utils.fillClassMap(batch,n_classes)
-
-      print('   # Computing means ')
-      for key in range(n_classes):
-        if key in range(n_classes-10,n_classes):
-          items = batch_map[key]
-        else:
-          items = exemplars[key]
-        
-        loader = DataLoader(items, batch_size=512, shuffle=False, num_workers=4, drop_last=False)
-        mean = torch.zeros((1,64),device=self.device)
-        for images, _ in loader:
-          with torch.no_grad():
-            images = images.to(self.device)
-            flipped_images = torch.flip(images,[3])
-            images = torch.cat((images,flipped_images))
-            
-            outputs = net(images,features=True)
-            
-            for output in outputs:
-              mean += output
-        mean = mean / ( 2 * len(items) ) 
-        means[key] = mean / mean.norm()
-
-      loader = DataLoader(data, batch_size=512, shuffle=False, num_workers=4, drop_last=False)
-
-      predictions, label_list = [], []
-      print('   # NME Predicting ')
-      for images, labels in loader:
-        images = images.to(self.device)
-        label_list += list(labels)
-        with torch.no_grad():
-          outputs = net(images,features=True)
-          for output in outputs:
-            prediction = None
-            min_dist = 99999
-            for key in means:
-              dist = torch.dist(means[key],output)
-              if dist < min_dist:
-                min_dist = dist
-                prediction = key
-            predictions.append(prediction)
-          
-      accuracy = accuracy_score(label_list,predictions)
-      print(f'   # NME Accuracy: {accuracy}')
-
-      return accuracy, predictions, label_list
-
-    def __FCClassifier__(self,data,net,n_classes):
-      print(f'\n ### FC Layer ###')
-      print('   # FC Layer Predicting ')
-      net.eval()
-      
-      running_corrects = 0.0
-      label_list, predictions = [], []
-      with torch.no_grad():
-        loader = DataLoader(data, batch_size=512, shuffle=False, num_workers=4, drop_last=False)
-        for images, labels in loader:
-          images = images.to(self.device)
-          labels = labels.to(self.device)
-
-          outputs = torch.sigmoid(net(images))
-          # Get predictions
-          _, preds = torch.max(outputs.data, 1)
-          # Update Corrects
-          running_corrects += torch.sum(preds == labels.data).data.item()
-          
-          for prediction,label in zip(preds,labels):
-            predictions.append(np.array(prediction.cpu()))
-            label_list.append(np.array(label.cpu()))
-
-        # Calculate Accuracy
-        accuracy = running_corrects / len(data)
-      
-      print(f'   # FC Layer Accuracy: {accuracy}')
-      return accuracy, predictions, label_list
  
     def __train__(self,data,exemplars,net,n_classes,stabilize=False):
         step = int(n_classes/10) - 1
@@ -120,7 +38,7 @@ class iCaRL2():
           milestones = set([ 49, 63 ])
           
           if len(exemplars) != 0:
-            data = data + self.__formatExemplars__(exemplars)
+            data = data + utils.formatExemplars(exemplars)
             # Save network for distillation
             old_net = deepcopy(net)
             old_net.eval()
@@ -133,7 +51,7 @@ class iCaRL2():
           EPOCHS = self.params['EPOCHS2']
           LR = self.params['LR2']
           milestones = set([ int(EPOCHS/3), int(2*EPOCHS/3) ])
-          data = self.__formatExemplars__(exemplars)
+          data = utils.formatExemplars(exemplars)
 
         # Define Loss
         criterion = MSELoss() 
@@ -198,45 +116,6 @@ class iCaRL2():
             current_outputs = self.teachers[i](images)[:,i*10:i*10+10]
             outputs = torch.cat((outputs,current_outputs),1)
       return outputs
-
-    def __randomExemplarSet__(self,data,n_classes):
-      print('\n ### Construct Random Exemplar Set ###')
-      if n_classes != 10:
-        m = int(self.memory/(n_classes-10))
-      else:
-        m = int(self.memory/(n_classes))
-      print(f'   # Exemplars per class: {m}')
-
-      # Initialize lists of images and exemplars for each class
-      class_map = utils.fillClassMap(data,n_classes)
-      exemplars = dict.fromkeys(np.arange(n_classes-10,n_classes))
-      for label in exemplars:
-        exemplars[label] = []
-
-      for label in class_map:
-        indexes = random.sample(range(len(class_map[label])),m)   
-        for idx in indexes:
-            exemplars[label].append(class_map[label][idx])
-
-      return exemplars
-  
-    def __reduceExemplarSet__(self,exemplars,n_classes):
-      print('\n ### Reduce Exemplar Set ###')
-      m = int(self.memory/n_classes)
-      print(f'   # Exemplars per class: {m}')
-      for key in exemplars:
-        exemplars[key] = exemplars[key][:m]
-      
-      return exemplars
-    
-    # dict to list
-    def __formatExemplars__(self,exemplars):
-      new_exemplars = []
-      for key in exemplars:
-        for item in exemplars[key]:
-          new_exemplars.append([item[0],item[1]])
-
-      return new_exemplars
       
     # Run ICaRL
     def run(self,train_batches,test_batches,net):
@@ -251,21 +130,22 @@ class iCaRL2():
         net = self.__train__(batch,exemplars,net,n_classes)
         utils.printTime(t0)
         
-        new_exemplars = self.__randomExemplarSet__(batch,n_classes)
+        # Exemplars managing
+        new_exemplars = utils.randomExemplarSet(self.memory,batch,n_classes)
         exemplars.update(new_exemplars)
         utils.printTime(t0)
         
+        # Stabilization 
         if idx != 0:
-          self.__FCClassifier__(test_batches[idx],net,n_classes)
+          utils.FCClassifier(test_batches[idx],net,n_classes)
           utils.printTime(t0)
           net = self.__train__([],exemplars,net,n_classes,stabilize=True)
           utils.printTime(t0)
         
-        # Classifier
-        self.__FCClassifier__(test_batches[idx],net,n_classes)
+        # Classification
+        utils.FCClassifier(test_batches[idx],net,n_classes)
         utils.printTime(t0)
-        accuracy, predictions, labels = self.__NMEClassifier__(test_batches[idx],batch,exemplars,net,n_classes)
-    
+        accuracy, predictions, labels = utils.NMEClassifier(test_batches[idx],batch,exemplars,net,n_classes)
         accuracy_per_batch.append(accuracy)
         utils.printTime(t0)
         
@@ -273,7 +153,7 @@ class iCaRL2():
           utils.confusionMatrix(labels,predictions,idx)
 
         # Exemplars managing
-        exemplars = self.__reduceExemplarSet__(exemplars,n_classes)
+        exemplars = utils.reduceExemplarSet(self.memory,exemplars,n_classes)
         utils.printTime(t0)
 
       return accuracy_per_batch
